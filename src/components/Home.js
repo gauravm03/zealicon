9047cc3245d843afd645b937a2970389000b3379
Gaurav Mishra <gauravmishras208013@gmail.com>
2023-04-26 05:06:28 +0530

    heyy

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import "./Home.css"
 const Home = () => {
   return (
-    <div className=''>
-        <div className='w-10/12 mb-12 flex flex-col gap-6 mx-auto pt-10 md:my-2'>
+    <div className='background'>
+        <div className=' w-10/12 mb-12 flex flex-col gap-6 mx-auto pt-10 md:my-2'>
           <div class="text-yellow-400 text-3xl font-bold">Entry Pass</div>
           <div className='text-white flex flex-col justify-center gap-5 items-center mx-auto'>
               <h3 className='text-3xl'>Naincy Tiwari</h3>
